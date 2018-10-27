9af0727aa833ecdc19a27a2402ccd9f05838d688
luisgilberto1995 <luisgil95crash@gmail.com>
2018-10-27 14:39:44 -0600

    Entorno y scripts necesarios

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+var ref_Publicaciones = firebase.database().ref('publicacion');
+
+(function () 
+{
+    var app = angular.module("app", ["firebase"]);
+
+    app.controller("ctrl", function($scope, $firebaseArray, $firebaseObject) {
+
+        // var ref = firebase.database().ref();
+        // download the data into a local object
+        // $scope.data = $firebaseObject(ref);
+        // putting a console.log here won't work, see below
+
+
+
+        $(document).ready(function () 
+        {
+            $scope.texto = "Hola Luis!";
+            var publicaciones = $firebaseArray(ref_Publicaciones);
+            publicaciones.$loaded().then(function()
+            {
+                publicaciones.forEach(p => 
+                {
+                    console.log(p);
+                });
+            });
+        });
+
+
+      });
+})()
